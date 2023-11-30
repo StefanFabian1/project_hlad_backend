d845326b2bc74d6a4b7fb7beb4ddfaf71ba37e4b
stefanfabian1 <stefan.fabian.159@gmail.com>
2023-11-30 16:41:55 +0100

    upratanie povodnych test classes

--- Model/TestModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once PROJECT_ROOT_PATH . "\\Model\\Database\\Database.php";
-
-    class TestModel extends Database {
-        public function getTestData() : array {
-            return $this->select("SELECT * FROM test ORDER BY id ASC");
-        }
-    }
-?>
